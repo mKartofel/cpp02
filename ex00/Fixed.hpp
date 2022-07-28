95c7843af3eb2624217eea282ef76fc7072c6761
Vincent Fiszbin <fiszbin.vincent@gmail.com>
2022-07-28 11:15:58 +0200

    comments

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: vfiszbin <vfiszbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 19:36:48 by vfiszbin          #+#    #+#             */
-/*   Updated: 2022/07/28 11:07:55 by vfiszbin         ###   ########.fr       */
+/*   Updated: 2022/07/28 11:15:14 by vfiszbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ private:
 	int _numValue;
 	static const int _nbFracBits = 8;
 public:
-	Fixed();
-	Fixed(Fixed const &fixed);
-	Fixed &operator=(Fixed const &fixed);
-	~Fixed();
+	Fixed(); //constructeur par défaut
+	Fixed(Fixed const &fixed); //constructeur de recopie
+	Fixed &operator=(Fixed const &fixed); //operateur d'affection
+	~Fixed(); //destructeur
 
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
